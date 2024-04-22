6ced08f0a95162e409ce2a06a4300ea10f756871
Yide Yu <mr_yideyu@163.com>
2024-04-22 11:25:22 +0800

    add desktop application

--- main.py
+++ main.py
@@ -20,4 +20,43 @@
 5. visualization
 '''
 
+'''
+6. desktop application
+'''
+
+
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+#
+#
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.init_ui()
+#
+#     def init_ui(self):
+#         self.setWindowTitle("Population natural deduction simulator")
+#         self.setGeometry(100, 100, 500, 300)
+#
+#         self.label = QLabel("Hello, PyQt!", self)
+#         self.label.setGeometry(50, 50, 200, 30)
+#
+#         self.button = QPushButton("Click Me", self)
+#         self.button.setGeometry(100, 100, 100, 30)
+#         self.button.clicked.connect(self.button_clicked)
+#
+#     def button_clicked(self):
+#         self.label.setText("Button Clicked!")
+#
+#
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec_())
+
+
+
+
+
 
